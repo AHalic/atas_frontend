b7fa4100e81b564b965a1125db29258ec9212ce1
AHalic <sophie.gama@aluno.ufes.br>
2022-11-19 23:59:37 -0300

    :truck: :construction: changes directory

--- app/src/Components/Form/Form.js
+++ app/src/Components/Form/Form.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import api from "./services/api";
+import Header from "../AppHeader/AppHeader";
+import api from "../../services/api";
 
 
 export default function Form() {
@@ -10,26 +11,16 @@ export default function Form() {
 			.get("/api/Locais")
 			.then(response => {
 				setLocal(response.data)
-				console.log(response.data)
 			})
 			.catch((err) => {
 				console.error("ops! ocorreu um erro" + err);
 			});
 	}, []);
-		
-	console.log('local', local[0].nome)
 
-	if (!local) {
-		return <p>Carregando...</p>;
-	}
 
 	return (
 		<div className="App">
-			<select id="local">
-				{local ? local.map((l) => (
-					<option key={`${l.id}`} value={l.id}>{l.nome}</option>
-				)) : <option key={'0'} value={0}>Sem local disponível</option>}
-			</select>
+			<Header />
 		</div>
 	);
 }
